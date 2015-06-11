3defd3ac0292b5bb34c0c5c59098f81192028f0a
Simon Bernard <sbernard@sierrawireless.com>
2015-06-11 12:06:06 +0200

    define formatter

--- main.cpp
+++ main.cpp
@@ -21,28 +21,25 @@
 #include "object_accelerometer.cpp"
 
 extern "C" {
-    #include "wakaama/liblwm2m.h"
+#include "wakaama/liblwm2m.h"
 }
-
 
 C12832 lcd(p5, p7, p6, p8, p11);
 
-extern "C"
-{
-    extern lwm2m_object_t * get_object_device();
-    extern lwm2m_object_t * get_object_firmware();
-    extern lwm2m_object_t * get_server_object(int serverId, const char* binding, int lifetime, bool storing);
-    extern lwm2m_object_t * get_security_object(int serverId, const char* serverUri, bool isBootstrap);
-    extern char * get_server_uri(lwm2m_object_t * objectP, uint16_t serverID);
+extern "C" {
+extern lwm2m_object_t * get_object_device();
+extern lwm2m_object_t * get_object_firmware();
+extern lwm2m_object_t * get_server_object(int serverId, const char* binding, int lifetime, bool storing);
+extern lwm2m_object_t * get_security_object(int serverId, const char* serverUri, bool isBootstrap);
+extern char * get_server_uri(lwm2m_object_t * objectP, uint16_t serverID);
 }
 
 // a linked logical connection to a server
-typedef struct _connection_t
-{
-    struct _connection_t *  next;
-    char *                  host;
-    int                     port;
-    Endpoint                ep;
+typedef struct _connection_t {
+    struct _connection_t * next;
+    char * host;
+    int port;
+    Endpoint ep;
 } connection_t;
 
 DigitalOut myled(LED1);
@@ -50,8 +47,7 @@ DigitalOut myled(LED1);
 // the server UDP socket
 UDPSocket udp;
 
-void ethSetup()
-{
+void ethSetup() {
     EthernetInterface eth;
     eth.init(); //Use DHCP
     eth.connect();
@@ -70,35 +66,33 @@ lwm2m_object_t * serverObject;
 connection_t * connList;
 
 /* create a new connection to a server */
-static void * prv_connect_server(uint16_t serverID, void * userData)
-{
+static void * prv_connect_server(uint16_t serverID, void * userData) {
     char * host;
     char * portStr;
     char * ptr;
     int port;
     connection_t * connP = NULL;
 
-    printf("Create connection for server %d\n",serverID);
+    printf("Create connection for server %d\n", serverID);
 
     // need to created a connection to the server identified by server ID
     char* uri = get_server_uri(securityObjP, serverID);
 
-    if (uri == NULL)
-    {
+    if (uri == NULL) {
         printf("server %d not found in security object\n", serverID);
         return NULL;
     }
     printf("URI: %s\n", uri);
 
     // parse uri in the form "coaps://[host]:[port]"
-    if (0==strncmp(uri, "coap://",  strlen("coap://"))){
-        host = uri+strlen("coap://");
-    }
-    else {
+    if (0 == strncmp(uri, "coap://", strlen("coap://"))) {
+        host = uri + strlen("coap://");
+    } else {
         goto exit;
     }
     portStr = strchr(host, ':');
-    if (portStr == NULL) goto exit;
+    if (portStr == NULL)
+        goto exit;
     // split strings
     *portStr = 0;
     portStr++;
@@ -110,9 +104,8 @@ static void * prv_connect_server(uint16_t serverID, void * userData)
     printf("Trying to connect to LWM2M Server at %s:%d\r\n", host, port);
 
     //  create a connection
-    connP =  (connection_t *)malloc(sizeof(connection_t));
-    if (connP == NULL)
-    {
+    connP = (connection_t *) malloc(sizeof(connection_t));
+    if (connP == NULL) {
         printf("Connection creation fail (malloc)\n");
         goto exit;
     } else {
@@ -124,42 +117,33 @@ static void * prv_connect_server(uint16_t serverID, void * userData)
         connList = connP;
         printf("udp connection created\n");
     }
-exit:
-    free(uri);
+    exit: free(uri);
 
     return connP;
 }
 
 /* send a buffer to a session*/
-static uint8_t prv_buffer_send(void * sessionH,
-                               uint8_t * buffer,
-                               size_t length,
-                               void * userdata)
-{
+static uint8_t prv_buffer_send(void * sessionH, uint8_t * buffer, size_t length, void * userdata) {
     printf("sending\n");
     connection_t * connP = (connection_t*) sessionH;
 
-    if (connP == NULL)
-    {
+    if (connP == NULL) {
         printf("#> failed sending %u bytes, missing connection\r\n", length);
-        return COAP_500_INTERNAL_SERVER_ERROR;
+        return COAP_500_INTERNAL_SERVER_ERROR ;
     }
 
-    printf("sending to %s\n",connP->ep.get_address());
+    printf("sending to %s\n", connP->ep.get_address());
 
-    	printf("send NO_SEC datagram\n");
-		if (-1 == udp.sendTo(connP->ep, (char*)buffer, length))
-		{
-			printf("send error\n");
-			return COAP_500_INTERNAL_SERVER_ERROR;
-		}
-	return COAP_NO_ERROR;
+    printf("send NO_SEC datagram\n");
+    if (-1 == udp.sendTo(connP->ep, (char*) buffer, length)) {
+        printf("send error\n");
+        return COAP_500_INTERNAL_SERVER_ERROR ;
+    }
+    return COAP_NO_ERROR ;
 }
 
-int main()
-{
+int main() {
     int result;
-
 
     lwm2m_object_t * objArray[5];
 
@@ -184,8 +168,7 @@ int main()
      * charge of communication
      */
     lwm2mH = lwm2m_init(prv_connect_server, prv_buffer_send, NULL);
-    if (NULL == lwm2mH)
-    {
+    if (NULL == lwm2mH) {
         fprintf(stderr, "lwm2m_init() failed\r\n");
         return -1;
     }
@@ -193,8 +176,7 @@ int main()
     // configure the liblwm2m lib
     result = lwm2m_configure(lwm2mH, "smart-watch", NULL, NULL, 5, objArray);
 
-    if (result != 0)
-    {
+    if (result != 0) {
         printf("lwm2m_configure() failed: 0x%X\n", result);
         return -1;
     }
@@ -202,8 +184,7 @@ int main()
     // start
 
     result = lwm2m_start(lwm2mH);
-    if (result != 0)
-    {
+    if (result != 0) {
         printf("lwm2m_start() failed: 0x%X\n", result);
         return -1;
     }
@@ -221,32 +202,31 @@ int main()
 
         time_t seconds = time(NULL);
         char buf[32];
-        lcd.locate(0,10);
+        lcd.locate(0, 10);
         strftime(buf, 32, "%x : %r", localtime(&seconds));
         lcd.printf("%s", buf);
 
         result = lwm2m_step(lwm2mH, &timeout.tv_sec);
-        if (result != 0)
-        {
+        if (result != 0) {
             printf("lwm2m_step error %d\n", result);
         }
         int n = udp.receiveFrom(server, buffer, sizeof(buffer));
         printf("Received packet from: %s of size %d\n", server.get_address(), n);
-        if (n>0) {
+        if (n > 0) {
             // TODO: find connection
             connection_t * connP = connList;
-            while(connP != NULL) {
-                if (strcmp(connP->host, server.get_address()) == 0)
-                {
+            while (connP != NULL) {
+                if (strcmp(connP->host, server.get_address()) == 0) {
 
                     printf("found connection\n");
                     // is it a secure connection?
                     printf("nosec session\n");
-					lwm2m_handle_packet(lwm2mH, (uint8_t*)buffer, n, (void*)connP);
-					break;
+                    lwm2m_handle_packet(lwm2mH, (uint8_t*) buffer, n, (void*) connP);
+                    break;
                 }
             }
-            if (connP == NULL) printf("no connection\n");
+            if (connP == NULL)
+                printf("no connection\n");
         }
         lwm2m_uri_t URI1;
         URI1.objectId = 3313;
@@ -254,13 +234,13 @@ int main()
         lwm2m_resource_value_changed(lwm2mH, &URI1);
 
         /*lwm2m_uri_t URI2;
-        URI2.objectId = 3313;
-        URI2.resourceId = 5703;
-        lwm2m_resource_value_changed(lwm2mH, &URI2);
+         URI2.objectId = 3313;
+         URI2.resourceId = 5703;
+         lwm2m_resource_value_changed(lwm2mH, &URI2);
 
-        lwm2m_uri_t URI3;
-        URI3.objectId = 3313;
-        URI3.resourceId = 5704;
-        lwm2m_resource_value_changed(lwm2mH, &URI3);*/
+         lwm2m_uri_t URI3;
+         URI3.objectId = 3313;
+         URI3.resourceId = 5704;
+         lwm2m_resource_value_changed(lwm2mH, &URI3);*/
     }
 }
