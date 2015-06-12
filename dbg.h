a68a564d51b550df756b2f1f9962ca4d4321123a
Simon Bernard <sbernard@sierrawireless.com>
2015-06-12 18:54:48 +0200

    fix dgb lib

--- dbg.h
+++ dbg.h
@@ -37,34 +37,28 @@ void debug_exact(const char* fmt, ...);
 
 #define DBG_SET_SPEED( x ) do{ debug_set_speed(x); }while(0)
 
-#if __DEBUG__ > 0
-#ifndef __MODULE__
-#define __MODULE__ __FILE__
-#endif
-#endif
-
 #if __DEBUG__ >= 1
-#define ERR(...) do{ debug(1, __MODULE__, __LINE__, __VA_ARGS__); }while(0)
+#define ERR(...) do{ debug(1, __FILE__, __LINE__, __VA_ARGS__); }while(0)
 #else
 #define ERR(...) do{ }while(0)
 #endif
 
 #if __DEBUG__ >= 2
-#define WARN(...) do{ debug(2, __MODULE__, __LINE__, __VA_ARGS__); }while(0)
+#define WARN(...) do{ debug(2, __FILE__, __LINE__, __VA_ARGS__); }while(0)
 #else
 #define WARN(...) do{ }while(0)
 #endif
 
 #if __DEBUG__ >= 3
-#define INFO(...) do{ debug(3, __MODULE__, __LINE__, __VA_ARGS__); }while(0)
-#define CHECK(ret) do{ if(ret){ debug_error(__MODULE__, __LINE__, ret); } }while(0)
+#define INFO(...) do{ debug(3, __FILE__, __LINE__, __VA_ARGS__); }while(0)
+#define CHECK(ret) do{ if(ret){ debug_error(__FILE__, __LINE__, ret); } }while(0)
 #else
 #define INFO(...) do{ }while(0)
 #define CHECK(ret) do{ }while(0)
 #endif
 
 #if __DEBUG__ >= 4
-#define DBG(...) do{ debug(4, __MODULE__, __LINE__, __VA_ARGS__); }while(0)
+#define DBG(...) do{ debug(4, __FILE__, __LINE__, __VA_ARGS__); }while(0)
 #define DBGX(...) do{ debug_exact(__VA_ARGS__); }while(0)
 #else
 #define DBG(...) do{ }while(0)
